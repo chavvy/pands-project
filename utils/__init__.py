df7504f1286e426a74e5c863c5017b938a5bb74e
chavvy <samedo1999@gmail.com>
2025-05-12 02:15:08 +0100

    updated functions to use lists for storing and printing information (for saving function later)

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from .data_functions import data_info, data_features_summary, data_features_summary_all
 from .data_loader import load_data
+from .file_saver import save_output
--- utils/data_functions.py
+++ utils/data_functions.py
@@ -5,9 +5,15 @@ import numpy as np
 #Functions for printing information on the iris dataset just for easier reading
 #originally there was a function for every variable, which can be seen in the example_functions.py
 def data_info(name, var):
-    print(f"\n{name} of the data:")
-    print(var)
+    lines = [] #list to store the information
+    lines.append(f"\n{name} of the data:\n")
+    lines.append(str(var))
+
+    #printing the final output
+    print("".join(lines))
+
     input("\nPress Enter to continue...")
+    return lines
 
 #functions for getting the first and last rows of a dataset
 #using variable n in case there's a need to get first or last 'n' number of rows instead of 5
@@ -35,18 +41,39 @@ def data_features_summary(features, features_names, stat="", pause=True):
     }
 
     function = stat_function[stat]
-    print(f"\n{stat.capitalize()} of each feature:\n")
-    
+    #adjusting code to fit with file_saver function
+    #makes it easier to save the output
+    header = f"{stat.capitalize()} of each feature:"
+    lines = []
+   
+    #adding a line of spacing with .append so it also appears in the saved output
+    lines.append("\n")
+    lines.append(header)
+    lines.append("\n")
+
     for r, name in enumerate(features_names):
         answer = function(features[:, r])
-        print(f"{name}: {answer:.2f}")
-    
+        #gets the summary of a feature, prints the summary, and then saves that to the lines list
+        line = f"{name}: {answer:.2f}\n" 
+        lines.append(line)
+
+    #printing the whole summary
+    print("".join(lines))
+
     if pause:
-        input("\nPress Enter to continue...")
+        input("Press Enter to continue...")
+    
+    return lines #stores the information to be used with other functions or whatever else
 
 def data_features_summary_all(features, features_names):
 
+    all_lines = []
+
     for stat in ["mean", "min", "max", "median", "std"]:
-        data_features_summary(features, features_names, stat, pause=False)
-        
+
+        lines = data_features_summary(features, features_names, stat, pause=False)
+        all_lines.extend(lines)
+
     input("\nPress Enter to continue...")
+
+    return all_lines
